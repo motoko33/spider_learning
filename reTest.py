8a81585ea4faa65569c221879295311ad083413a
motoko33 <59129451+motoko33@users.noreply.github.com>
2020-05-07 20:54:34 +0800

    Add files via upload

---
+++ reTest.py
@@ -0,0 +1,41 @@
+import re
+
+content = 'http://weibo.com/comment/kEraCN'
+result1 = re.match('http.*?comment/(.*?)', content)
+result2 = re.match('http.*?comment/(.*)', content)
+print('result1', result1.group(1))
+print('result2', result2.group(1))
+
+html = '''<div id="songs-list">
+<h2 class="title">经典老歌</h2>
+<p class="introduction">
+经典老歌列表
+</p>
+<ul id="list" class="list-group">
+<li data-view="2">一路上有你</li>
+<li data-view="7">
+<a href="/2.mp3" singer="任贤齐">沧海一声笑</a>
+</li>
+<li data-view="4" class="active">
+<a href="/3.mp3" singer="齐秦">往事随风</a>
+</li>
+<li data-view="6"><a href="/4.mp3" singer="beyond">光辉岁月</a></li>
+<li data-view="5"><a href="/5.mp3" singer="陈慧琳">记事本</a></li>
+<li data-view="5">
+<a href="/6.mp3" singer="邓丽君">但愿人长久</a>
+</li>
+</ul>
+</div>'''
+
+results = re.findall('<li.*?href="(.*?)".*?singer="(.*?)">(.*?)</a>', html, re.S)
+print(results)
+print(type(results))
+for result in results:
+    print(result)
+    print(result[0], result[1], result[2])
+
+html = re.sub('<a.*?>|</a>', '', html)
+print(html)
+results = re.findall('<li.*?>(.*?)</li>', html, re.S)
+for result in results:
+    print(result.strip())
